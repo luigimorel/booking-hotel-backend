9d99a1d64ac19f782473ebfae5c1846d6306d7ed
Luigi Morel <morelmiles@gmail.com>
2022-08-23 15:59:06 +0300

    add controllers

--- controllers/User.go
+++ controllers/User.go
@@ -14,22 +14,34 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	var users []models.User
 
 	config.DB.Find(&users)
-
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&users)
 }
 
 // GetUserById ... Get one user by id
 func GetUserById(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
+	userId := mux.Vars(r)["id"]
+	if !checkIfUserExists(userId) {
+		json.NewEncoder(w).Encode("user not found!")
+		return
+	}
 	var user models.User
-
-	config.DB.First(&user, params["id"])
-
+	config.DB.First(&user, userId)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
 
+func checkIfUserExists(userId string) bool {
+	var user models.User
+	config.DB.First(&user, userId)
+
+	return user.ID != 0
+}
+
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	var user models.User
 
 	json.NewDecoder(r.Body).Decode(&user)
@@ -42,4 +54,33 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	} else {
 		json.NewEncoder(w).Encode(&user)
 	}
+}
+
+func UpdateUserById(w http.ResponseWriter, r *http.Request) {
+	userId := mux.Vars(r)["id"]
+	if !checkIfUserExists(userId) {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("user not found!")
+		return
+	}
+	var user models.User
+	config.DB.First(&user, userId)
+	json.NewDecoder(r.Body).Decode(&user)
+	config.DB.Save(&user)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+func DeleteUserById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	userId := mux.Vars(r)["id"]
+	if !checkIfUserExists(userId) {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("user not found!")
+		return
+	}
+
+	var user models.User
+	config.DB.Delete(&user, userId)
+	json.NewEncoder(w).Encode(user)
 }
