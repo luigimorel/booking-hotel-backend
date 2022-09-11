aa7a95ccc3ae0291c5f1b70517c93124c0b6dc78
Luigi Morel <morelmiles@gmail.com>
2022-09-11 23:04:37 +0300

    update database fields

--- models/Property.go
+++ models/Property.go
@@ -9,15 +9,15 @@ import (
 
 type Property struct {
 	gorm.Model
-	HostName    User
-	CheckInTime time.Time      `gorm:"size:255;not null;default:flexible" json:"checkin_time"`
+	Owner       User           `gorm:"foreignKey:id;references:ID" json:"owner_id,omitempty"`
+	CheckInTime time.Time      `gorm:"size:255;not null;default:current_timestamp" json:"checkin_time"`
 	Guests      uint32         `gorm:"size:255;not null;" json:"guests"`
 	Beds        uint32         `gorm:"size:255;not null;" json:"beds"`
 	BedRooms    uint32         `gorm:"size:255;not null;" json:"bedrooms"`
 	Bathrooms   uint32         `gorm:"size:255;not null;" json:"bathrooms"`
-	Amenities   pq.StringArray `gorm:"size:255;not null;" json:"amenities"`
-	Description string         `gorm:"type:text;not null;" json:"description"`
+	Amenities   pq.StringArray `gorm:"type:text[];not null;" json:"amenities"`
+	Description string         `gorm:"type:text;" json:"description"`
 	Location    string         `gorm:"size:255;not null;" json:"location"`
-	HouseRules  pq.StringArray `gorm:"size:255;" json:"houseRules"`
-	Images      pq.StringArray `gorm:"type:text;" json:"images"`
+	HouseRules  pq.StringArray `gorm:"type:text[];" json:"houseRules"`
+	Images      pq.StringArray `gorm:"type:text[];" json:"images"`
 }
