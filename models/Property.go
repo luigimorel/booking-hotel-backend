90f542875a0e22bf6adb8f4d6dae8dea4058abef
Luigi Morel <morelmiles@gmail.com>
2022-10-15 00:00:58 +0300

    add more fields

--- models/Property.go
+++ models/Property.go
@@ -3,23 +3,24 @@ package models
 import (
 	"errors"
 
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Property struct {
 	gorm.Model
-	CheckInTime  string    `gorm:"size:255;not null;default:current_timestamp" json:"checkin_time"`
-	Guests       uint32    `gorm:"size:255;not null;" json:"guests"`
-	Beds         uint32    `gorm:"size:255;not null;" json:"beds"`
-	BedRooms     uint32    `gorm:"size:255;not null;" json:"bedrooms"`
-	Bathrooms    uint32    `gorm:"size:255;not null;" json:"bathrooms"`
-	Description  string    `gorm:"type:text;" json:"desc"`
-	Location     string    `gorm:"size:255;not null;" json:"location"`
-	Images       string    `gorm:"type:text;" json:"images"`
-	Owner        User      `json:"owner"`
-	OwnerID      uint32    `sql:"type:int; REFERENCES users(id)" json:"owner_id"`
-	HouseRules   HouseRule `gorm:"house_rules"`
-	HouseRulesID uint32    `gorm:"type:int REFERENCES house_rules(id)" json:"rule_id"`
+	CheckInTime string         `gorm:"size:255;not null;default:current_timestamp" json:"checkin_time"`
+	Guests      uint32         `gorm:"size:255;not null;" json:"guests"`
+	Beds        uint32         `gorm:"size:255;not null;" json:"beds"`
+	BedRooms    uint32         `gorm:"size:255;not null;" json:"bedrooms"`
+	Bathrooms   uint32         `gorm:"size:255;not null;" json:"bathrooms"`
+	Description string         `gorm:"type:text;" json:"desc"`
+	Location    string         `gorm:"size:255;not null;" json:"location"`
+	Images      string         `gorm:"type:text;" json:"images"`
+	Owner       User           `json:"owner"`
+	OwnerID     uint32         `gorm:"not null;" json:"owner_id"`
+	HouseRules  []HouseRule    `gorm:"many2many:house_rules;"`
+	Tags        pq.StringArray `gorm:"type:varchar(64)[]"`
 }
 
 func (p *Property) Validate() error {
