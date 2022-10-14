4b64318d68594f2f002f5e7c3a90d36727f1f7fa
Luigi Morel <morelmiles@gmail.com>
2022-10-14 23:38:50 +0300

    fix property model

--- models/Property.go
+++ models/Property.go
@@ -2,14 +2,13 @@ package models
 
 import (
 	"errors"
-	"time"
 
 	"gorm.io/gorm"
 )
 
 type Property struct {
 	gorm.Model
-	CheckInTime  time.Time `gorm:"size:255;not null;default:current_timestamp" json:"checkin_time"`
+	CheckInTime  string    `gorm:"size:255;not null;default:current_timestamp" json:"checkin_time"`
 	Guests       uint32    `gorm:"size:255;not null;" json:"guests"`
 	Beds         uint32    `gorm:"size:255;not null;" json:"beds"`
 	BedRooms     uint32    `gorm:"size:255;not null;" json:"bedrooms"`
@@ -18,9 +17,9 @@ type Property struct {
 	Location     string    `gorm:"size:255;not null;" json:"location"`
 	Images       string    `gorm:"type:text;" json:"images"`
 	Owner        User      `json:"owner"`
-	OwnerID      uint32    `sql:"type:int REFERENCES users(id)" json:"owner_id"`
-	HouseRules   HouseRule `json:"house_rules"`
-	HouseRulesID uint32    `sql:"type:int REFERENCES house_rules(id)" json:"rule_id"`
+	OwnerID      uint32    `sql:"type:int; REFERENCES users(id)" json:"owner_id"`
+	HouseRules   HouseRule `gorm:"house_rules"`
+	HouseRulesID uint32    `gorm:"type:int REFERENCES house_rules(id)" json:"rule_id"`
 }
 
 func (p *Property) Validate() error {
